95eca39ea5e67967857f2d7420cfd28b619dfd48
Samarth <samarth212@gmail.com>
2024-01-16 12:40:23 -0500

    minor js progress

--- ec.js
+++ ec.js
@@ -1,9 +1,27 @@
 function addEC(){
     document.querySelector(".ec").innerHTML += `
     
-    <p>hello</p>
-    
-    
-    
+    <div class="ec-row">
+
+        <div class="ec-dropdown">
+
+            <button class="ec-type-ph">Type</button>
+
+            <div class="ec-type">
+                <p>School club</p>
+                <p>School sport</p>
+                <p>Internship/Research</p>
+                <p>Volunteering</p>
+                <p>Job</p>
+            </div>
+
+        </div>
+                
+    </div>
+       
     `
+}
+
+function changeType(type){
+    document.querySelector(".ec-type").innerHTML = 'type'
 }
