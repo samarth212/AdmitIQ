38c562adbd12b65d3104fa5ae9180c5a47ada37d
Samarth <samarth212@gmail.com>
2024-01-16 19:28:45 -0500

    finsihed func for regional awards

--- home.js
+++ home.js
@@ -24,6 +24,44 @@ function calcStats(){
         console.log(gpaPoints)
     }
 
+    let sat = Number(document.querySelector('.sat').value)
+    let satPoints = 0
+
+    if (isNaN(sat)){
+        alert('Please enter a valid SAT Score.')
+    }
+    else{
+        if (sat >= 1550){
+            satPoints = 5
+        }
+        else if (sat >= 1500){
+            satPoints = 3
+        }
+        else if (sat >= 1400){
+            satPoints = 1
+        }
+        else{
+            satPoints = 0
+        }
+    }
+
+    console.log(satPoints)
+
+    let ra = Number(document.querySelector('.regional-awards').value)
+    let raPoints = 0
+
+    if (isNaN(ra)){
+        alert('Please enter a valid input for regional awards.')
+    }
+    else{
+        raPoints = ra
+    }
+    console.log(raPoints)
+
     
+
+}
+
+function calcEC(){
 
 }
